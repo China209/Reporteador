69bb1c43532116b8cedddb2d2cfa6b8c84f58cdd
CYPAMR99 <cmedinar1@miumg.edu.gt>
2020-11-04 15:04:20 -0600

    Con Pruebas para Ejecutar

---
+++ Version Final/Reporteador/CapaVista/ReportesModulos/frmReportesModulos.cs
@@ -0,0 +1,60 @@
+﻿/*
+ Muestra los reportes por modulo y permite visualizarlos en una ventana emergente
+ */
+using CapaControladorReporteador.ControladoresReporteador;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaVistaReporteador.ReportesModulos
+{
+    public partial class frmReportesModulos : Form
+    {
+        clsControlModuloReporte controlModuloReporte = new clsControlModuloReporte();
+        int IDModuloAux;
+        string RutaReporte;
+        public frmReportesModulos(int IDModulo)
+        {
+            InitializeComponent();
+            this.IDModuloAux = IDModulo;
+            if (IDModulo >= 1)
+            {
+                cargarGrid();
+            }
+            tltVentana.SetToolTip(dgvReportes, "Seleccione el reporte que desea ver");
+        }
+
+        private void cargarGrid()
+        {
+            dgvReportes.DataSource=controlModuloReporte.obtenerTodo(this.IDModuloAux);
+        }
+
+        private void mostrarReporteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmMostraModulos mostraModulos = new frmMostraModulos(RutaReporte);
+            mostraModulos.Show();
+        }
+
+        private void dgvReportes_CellMouseUp(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                this.RutaReporte = dgvReportes.Rows[e.RowIndex].Cells["Ruta Reporte"].Value.ToString();
+                this.cmsMostrar.Show(this.dgvReportes, e.Location);
+                cmsMostrar.Show(Cursor.Position);
+            }
+        }
+
+        private void tmrHoraFecha_Tick(object sender, EventArgs e)
+        {
+            lblHora.Text = DateTime.Now.ToLongTimeString();
+            lblFecha.Text = DateTime.Now.ToLongDateString();
+        }
+    }
+}
